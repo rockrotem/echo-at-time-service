c6e4a08665e5139889358944d76785fb686ad790
rotembloom <rotemb@indeni.com>
2018-10-13 16:03:42 +0300

    final version

--- README.md
+++ README.md
@@ -1,5 +1,38 @@
 # echo-at-time-service
-A service for sending a delayed jobs with Redis as persistence storage.
+A service for sending a delayed jobs. It uses Redis and its data types in order to achieve:
+1. Persistence
+2. Reliability
+3. Performance
+4. Scalability
+
+## High level architecture
+The service has 1 API POST call:
+/api/v1/save-message with the payload:
+```javascript
+const message = {
+  "message": "Benec can.",
+  "runAt": 1539430684913,
+  "uuid": "34ec6bfa-0df6-4465-b09e-c63b3121466c"
+};
+```
+
+It uses 3 Redis Queue:
+1. QueueDelayed - Store messages should be processed in future (delayed)
+2. Queue - Store messages to process immediately (now)
+3. QueueDead - Store messages to process in case service was down unexpectedly (reliability);
+
+### Flow logic
+1. Message should run now saved to redis Queue: and processed immediately.
+2. Message should run in future saved to redis QueueDelayed: and processed when needed by runAt.
+   When message time to process arrived it will deleted from QueueDelayed: and move to Queue: with redis distributed lock (redlock).
+   
+Reliability and only one server message processing will be implemented using redis: rpoplpush command.
+
+### Future architecture improvements
+* Split read and write services:
+    1. Service write message to redis (micro-service only writes)
+    2. Service worker for each queue (micro-service only read process messages, dead messages)
+* Can implement master election using zookeeper or redis so only one server will process messages with no locking.
 
 # installation
 ```bash
@@ -8,15 +41,22 @@ npm i
 
 # package dependencies
 ## dependencies
-* express http server
-* redis as redis client
+* express - as the http server
+* redis - as redis client
+* redlock - as a redis distributed lock.
+* bluebird - as promise library for redis (bluebird.promisifyAll(redis))
 
 ## dev dependencies
-* eslint + airbnb code style
-* mocha + chai for TDD and testing
+* eslint + airbnb - code style
+* mocha + chai - TDD and testing
+* chance - generates message text randomly 
 
 # run tests
-Will run all tests + code style using eslint
+Will run all tests + code style using eslint.
+2 end to end tests:
+* Post to service and add 2 messages to Delayed Queue.
+* Post to service and add 1 message to Process Queue.
+
 
 ```bash
 npm test
--- test/end2end/routes/echo-in-time-route-test.js
+++ test/end2end/routes/echo-in-time-route-test.js
@@ -1,11 +1,13 @@
 // require('../../e2e-tools/setup');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const Chance = require('chance');
 const { describe, before, it } = require('mocha');
 const MessageModel = require('../../../lib/models/message-model');
 
 chai.use(chaiHttp);
 const { expect } = chai;
+const chance = new Chance();
 let app;
 
 describe('echo-in-time route tests', () => {
@@ -15,23 +17,42 @@ describe('echo-in-time route tests', () => {
   });
 
   describe('Save message POST action', () => {
-    it('should save message with valid payload', async () => {
-      const message = 'test message';
-      const runAt = new Date().getTime();
-      const messageModel = new MessageModel(message, runAt);
-      const response = await chai.request(app).post('/api/v1/save-message')
+    it('should save 2 messages to delayed queue', async () => {
+      let message = chance.sentence({ words: 3 });
+      let runAt = new Date();
+      runAt.setSeconds(runAt.getSeconds() + 10);
+      let messageModel = new MessageModel(message, runAt.getTime());
+      let response = await chai.request(app).post('/api/v1/save-message')
         .set('content-type', 'application/json')
         .send(messageModel);
 
       const { body } = response;
       expect(response.status, 'HTTP status').to.equal(200);
-      expect(body.ok, 'body.ok').to.equal(true);
+      expect(body, 'body').to.eql({
+        message: messageModel,
+        ok: true,
+      });
+
+      message = chance.sentence({ words: 2 });
+      runAt = new Date();
+      runAt.setSeconds(runAt.getSeconds() + 60);
+      messageModel = new MessageModel(message, runAt.getTime());
+      response = await chai.request(app).post('/api/v1/save-message')
+        .set('content-type', 'application/json')
+        .send(messageModel);
+
+      const body2 = response.body;
+      expect(response.status, 'HTTP status').to.equal(200);
+      expect(body2, 'body').to.eql({
+        message: messageModel,
+        ok: true,
+      });
     });
 
-    it('should save messages with valid payload and future timestemp', async () => {
-      const message = 'test message';
+    it('should save message to processing queue', async () => {
+      const message = chance.sentence({ words: 5 });
       const runAt = new Date();
-      runAt.setSeconds(runAt.getSeconds() + 60);
+      runAt.setSeconds(runAt.getSeconds() - 60);
       const messageModel = new MessageModel(message, runAt.getTime());
       const response = await chai.request(app).post('/api/v1/save-message')
         .set('content-type', 'application/json')
@@ -39,7 +60,10 @@ describe('echo-in-time route tests', () => {
 
       const { body } = response;
       expect(response.status, 'HTTP status').to.equal(200);
-      expect(body.ok, 'body.ok').to.equal(true);
+      expect(body, 'body').to.eql({
+        message: messageModel,
+        ok: true,
+      });
     });
   });
 });
